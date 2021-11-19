3eeb9e13b5d571d30534d210342a266bf96f82be
MilagrosAlbarengo <92064009+MilagrosAlbarengo@users.noreply.github.com>
2021-11-18 21:48:10 -0300

    Initial commit

---
+++ src/añobisiesto.ts
@@ -0,0 +1,16 @@
+let meses : number [] = new Array ([ = 31;28;31;30;31;30;31;31;30;31;30]);
+let fecha : number = 0;
+let mes : number = 0;
+let anio: number = 0;
+let bisiesto : boolean = false;
+let diasDelAnio : number = 365;
+let diasTranscurridos: number = 0;
+let diasParaFindeAnio: number = 0;
+// anio = Number (prompt("Ingrese año"));
+if ((anio%4===0 && anio %100 !==0)[anio%400===0]);{
+  bisiesto = true;
+  diasDelAnio= 365 + 1;
+  meses [1]= 29;
+}
+//mes= Number (prompt("Ingrese mes"));
+while(mes < 1  ) 
